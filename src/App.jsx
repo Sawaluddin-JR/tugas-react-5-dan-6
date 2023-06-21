d4b125d1ee714497fcf1602726d33c8ee2efdf20
Sawaluddin <sawaluddinsrg472@gmail.com>
2023-06-21 22:08:37 +0700

    SecondCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Galaxy from './components/Galaxy';
-import {RiDeleteBin6Line} from "react-icons/ri";
-import {AiOutlinePlus} from "react-icons/ai";
-import {BsArrowsFullscreen} from "react-icons/bs";
-import {HiOutlineArrowsPointingIn} from "react-icons/hi2"
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsArrowsFullscreen } from "react-icons/bs";
+import { HiOutlineArrowsPointingIn } from "react-icons/hi2"
 
 export default function App() {
   const [galaxies, setGalaxies] = useState([
     {
       id: 1,
-      name: "Andromeda",
-      diameter: 220000
+      name: "Magellan",
+      diameter: 14000
     },
     {
       id: 2,
-      name: "Bima Sakti",
-      diameter: 100000
+      name: "Mata Hitam",
+      diameter: 52962
     },
     {
       id: 3,
-      name: "Triangulum",
-      diameter: 60000
+      name: "Sombrero",
+      diameter: 61000
+    },
+    {
+      id: 4,
+      name: "Bima Sakti",
+      diameter: 100000
     }
   ]);
 
@@ -33,11 +38,54 @@ export default function App() {
 
   const [EditById, setEditById] = useState(1);
 
+  const addGalaxyStart = (e) => {
+    e.preventDefault();
+    setGalaxies([newGalaxy, ...galaxies]);
+    setNewGalaxy({ id: [...galaxies, newGalaxy].length + 1, name: "", diameter: 0 });
+  }
+
+  const addGalaxyEnd = (e) => {
+    e.preventDefault();
+    setGalaxies([...galaxies, newGalaxy]);
+    setNewGalaxy({ id: [...galaxies, newGalaxy].length + 1, nama: "", diameter: 0 });
+  }
+
+  const EditNameGalaxy = (e) => {
+    galaxies.map((x) => {
+      x.id == EditById ?
+        x.name = e.target.value : galaxies;
+      setGalaxies([...galaxies]);
+    });
+  }
+
+  const EditPlusDiameterGalaxy = (e) => {
+    e.preventDefault();
+    setGalaxies(
+      galaxies.map((galaxy) =>
+        galaxy.id == EditById ?
+          { ...galaxy, diameter: galaxy.diameter + 1 } : galaxy)
+    );
+  }
+
+  const EditMinDiameterGalaxy = (e) => {
+    e.preventDefault();
+    setGalaxies(
+      galaxies.map((galaxy) =>
+        galaxy.id == EditById ?
+          { ...galaxy, diameter: galaxy.diameter + 1 } : galaxy)
+    );
+  }
+
+  const DeleteByIndexGalaxy = (e) => {
+    e.preventDefault();
+    setGalaxies(galaxies.filter((galaxy) => galaxy.id != EditById));
+  }
+
   return (
     <div>
       <Header />
       <main className="container">
-          {/* Untuk menampilkan objek galaxy */}
+        {/* Untuk menampilkan objek galaxy */}
         <div className="show-galaxy">
           {
             galaxies.map((galaxie, dex) => (
@@ -67,23 +115,15 @@ export default function App() {
           <label >
             Diameter :
             <input type="number" value={newGalaxy.diameter}
-              onChange={(e) => setNewGalaxy({...newGalaxy,diameter: parseInt(e.target.value)})}
+              onChange={(e) => setNewGalaxy({ ...newGalaxy, diameter: parseInt(e.target.value) })}
             />
           </label>
           <div className="add-content">
-            <button onClick={(e) => {
-              e.preventDefault();
-              setGalaxies([newGalaxy, ...galaxies]);
-              setNewGalaxy({ id: [...galaxies, newGalaxy].length + 1, name: "", diameter: 0 });
-            }}>
+            <button onClick={addGalaxyStart}>
               <AiOutlinePlus size={18} />  Depan
             </button>
-            <button onClick={(e) => {
-              e.preventDefault();
-              setGalaxies([...galaxies, newGalaxy])
-              setNewGalaxy({ id: [...galaxies, newGalaxy].length + 1, name: "", diameter: 0 });
-            }}>
-              <AiOutlinePlus size={16}/>  Belakang
+            <button onClick={addGalaxyEnd}>
+              <AiOutlinePlus size={16} />  Belakang
             </button>
           </div>
           {/* Untuk mengedit ataupun menghapus data berdasarkan id */}
@@ -96,43 +136,20 @@ export default function App() {
           </label>
           <label>
             Nama :
-            <input 
+            <input
               value={galaxies.find((x) => x.id == EditById)?.name ?? ""}
-              onChange={(e) => {
-                galaxies.map((x) => {
-                  x.id == EditById ? 
-                    x.name = e.target.value : galaxies;
-                    setGalaxies([...galaxies]);
-                });
-              }} />
+              onChange={EditNameGalaxy} />
           </label>
           <label >
             Diameter :
             <div className="diameter">
-              <button onClick={(e) => {
-                e.preventDefault();
-                setGalaxies(
-                  galaxies.map((galaxy) =>
-                    galaxy.id == EditById ?
-                      { ...galaxy, diameter: galaxy.diameter + 1 } : galaxy)
-                );
-              }}
-              > <BsArrowsFullscreen size={14}/> Perbesar
+              <button onClick={EditPlusDiameterGalaxy}
+              > <BsArrowsFullscreen size={14} /> Perbesar
               </button>
-              <button onClick={(e) => {
-                e.preventDefault();
-                setGalaxies(
-                  galaxies.map((galaxy) =>
-                    galaxy.id == EditById ?
-                      { ...galaxy, diameter: galaxy.diameter - 1 } : galaxy)
-                );
-              }}><HiOutlineArrowsPointingIn size={18}/> Perkecil</button>
+              <button onClick={EditMinDiameterGalaxy}><HiOutlineArrowsPointingIn size={18} /> Perkecil</button>
             </div>
           </label>
-          <button onClick={(e) => {
-            e.preventDefault();
-            setGalaxies(galaxies.filter((galaxy) => galaxy.id != EditById));
-          }} > <RiDeleteBin6Line size={16} />  Hapus
+          <button onClick={DeleteByIndexGalaxy} > <RiDeleteBin6Line size={16} />  Hapus
           </button>
         </form>
         <div className="delete-content">
